01ca6174178c87e9d5a4033e4f033893b24eaf5c
Taisuke Fukuno <fukuno@jig.jp>
2024-06-18 09:26:49 +0900

    remove unused code

--- downloadItems.js
+++ downloadItems.js
@@ -10,13 +10,11 @@ const itemnames = [
 ];
 
 const baseurl = "https://www1.city.tamba.lg.jp/ja/api/v1.0/";
-const data = {};
 for (const name of itemnames) {
   const url = baseurl + name + (name == "Datastreams" ? "?$top=200" : "");
   const json = JSON.parse(await fetchOrLoad(url));
   const lname = name.toLowerCase();
   await Deno.writeTextFile("data/" + lname + ".json", JSON.stringify(json, null, 2));
-  data[lname] = json;
 }
 
 //const url = "https://www1.city.tamba.lg.jp/ja/api/v1.0/Observations?$top=10";
